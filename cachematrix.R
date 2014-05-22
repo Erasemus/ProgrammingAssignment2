e4430f160c2f2a7879c9ba20a78bece9d2262acc
Erasemus <Randy.Catoe@gmail.com>
2014-05-22 08:46:10 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -16,7 +16,6 @@ m <- NULL
              setinverse = setinverse,
              getinverse = getinverse)
 }
-}
 ## This function computes the inverse of the special
 ## "matrix" returned by `makeCacheMatrix` above. If the inverse has
 ## already been calculated (and the matrix has not changed), then the
